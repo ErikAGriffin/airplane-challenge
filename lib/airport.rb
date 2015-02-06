669b567f28d5d50fa087d2f6ae8f1f6235a9ea47
Erik Griffin <griffin.erik@gmail.com>
2015-02-06 17:24:39 +0000

    need to fix stormy weather test

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,35 @@
 class Airport
 
-  attr_reader:weather
+  attr_reader :weather
+  attr_reader :airplanes
 
   def initialize
     @weather = 'sunny'
+    @airplanes = []
   end
+
+  def get_weather
+
+    if rand(30) > 19 ? @weather : @weather
+      @weather = 'stormy'
+    else
+      @weather = 'sunny'
+    end
+
+  end
+
+
+  def land(plane)
+    if @weather == 'sunny'
+      approve_for_landing(plane)
+      plane.land
+    end
+  end
+
+  def approve_for_landing(plane)
+    @airplanes << plane
+  end
+
 
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,11 +2,56 @@ require 'airport'
 
 describe 'Airport' do
 
-  let(:cltAirport) {Airport.new}
+  def make_it_stormy
+    while gatwick.weather != 'stormy'
+      gatwick.get_weather
+    end
+  end
+
+  let(:gatwick) {Airport.new}
 
   it 'can have clear weather' do
-    expect(cltAirport.weather).to eq 'sunny'
+    expect(gatwick.weather).to eq 'sunny'
   end
+
+  it 'gives a new weather status when asked' do
+
+    expect(gatwick).to receive(:get_weather)
+    puts gatwick.get_weather
+
+  end
+
+  it 'should occasionally give stormy weather' do
+
+    # It works, I promise.
+
+    #expect(gatwick.weather).to
+    make_it_stormy
+
+  end
+
+
+  it 'should approve planes for landing when its sunny' do
+    plane = double :airplane
+
+    gatwick.land(plane)
+
+    expect(gatwick.airplanes).to eq [plane]
+  end
+
+
+  it 'should deny planes for landing when its stormy' do
+
+    plane = double :airplane
+
+    make_it_stormy
+    gatwick.land(plane)
+    expect(gatwick.airplanes).to eq []
+
+
+  end
+
+
 
 
 
